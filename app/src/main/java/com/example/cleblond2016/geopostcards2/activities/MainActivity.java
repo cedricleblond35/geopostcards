619d96a43e1f6481117fc9e488f08768afc23f22
cleblond2016 <cedric.leblond35@gmail.com>
2018-07-10 13:25:28 +0200

    merge DAOBO
    # Conflicts:
    #	app/src/main/java/com/example/cleblond2016/geopostcards2/activities/MainActivity.java

--- app/src/main/java/com/example/cleblond2016/geopostcards2/activities/MainActivity.java
+++ app/src/main/java/com/example/cleblond2016/geopostcards2/activities/MainActivity.java
@@ -1,4 +1,4 @@
-package com.example.cleblond2016.geopostcards2;
+package com.example.cleblond2016.geopostcards2.activities;
 
 import android.Manifest;
 import android.content.Intent;
@@ -10,17 +10,16 @@ import android.location.LocationProvider;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v4.app.ActivityCompat;
-import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.TextView;
 import android.widget.Toast;
+
+import com.example.cleblond2016.geopostcards2.R;
 
 import org.osmdroid.api.IMapController;
 import org.osmdroid.tileprovider.tilesource.TileSourceFactory;
@@ -66,10 +65,10 @@ public class MainActivity extends AppCompatActivity implements LocationListener 
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(MainActivity.this, CreatePostCardActivity.class);
-                intent.putExtra("latitude", latitude);
-                intent.putExtra("longitude", longitude);
-                startActivity(intent);
+//                Intent intent = new Intent(MainActivity.this, CreatePostCardActivity.class);
+//                intent.putExtra("latitude", latitude);
+//                intent.putExtra("longitude", longitude);
+//                startActivity(intent);
 
 //                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
 //                        .setAction("Action", null).show();
